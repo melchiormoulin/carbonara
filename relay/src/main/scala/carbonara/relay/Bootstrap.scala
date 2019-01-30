92e121ff6af6f85d2a38ac6b1a21e2204e986995
Alban Dericbourg <a.dericbourg@criteo.com>
2019-01-30 16:52:08 +0100

    Return context on bootstrap
    The main goal is to make the Prometheus registry configurable and
    injectable (and not explicitly using the hidden static default).
    
    It allows to test more easily future implementations, including their
    interactions with the metrics library.
    
    Signed-off-by: Alban Dericbourg <a.dericbourg@criteo.com>

--- relay/src/main/scala/carbonara/relay/Bootstrap.scala
+++ relay/src/main/scala/carbonara/relay/Bootstrap.scala
@@ -1,14 +1,23 @@
 package carbonara.relay
 
+import java.net.InetSocketAddress
+
+import io.prometheus.client.CollectorRegistry
 import io.prometheus.client.exporter.HTTPServer
 
+case class CNContext(registry: CollectorRegistry)
+
 object Bootstrap {
-  def run(args: Array[String]): Unit = {
-    configureMetrics()
+  def run(args: Array[String]): CNContext = {
+    val registry = CollectorRegistry.defaultRegistry
+    configureMetrics(registry)
+
+    CNContext(registry)
   }
 
-  private def configureMetrics() = {
-    val server = new HTTPServer("0.0.0.0", 7080, true)
+  private def configureMetrics(registry: CollectorRegistry) = {
+    val socketAddress = new InetSocketAddress("0.0.0.0", 7080)
+    val server = new HTTPServer(socketAddress, registry, true)
     sys.addShutdownHook {
       server.stop()
     }
